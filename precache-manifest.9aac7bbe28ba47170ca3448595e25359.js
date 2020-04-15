e302c379254baa6f1fb10c2bb26473bbefdcae95
syafiqnkholis <syafiqnkh@gmail.com>
2020-04-15 14:44:22 +0700

    Updates

---
+++ precache-manifest.9aac7bbe28ba47170ca3448595e25359.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e81920de8e6c957a58b921fe1f76120",
+    "url": "/utsWeb/index.html"
+  },
+  {
+    "revision": "fa3cd281e64d1e4b56c2",
+    "url": "/utsWeb/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "68a859cbfbd11c1ef2c4",
+    "url": "/utsWeb/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fa3cd281e64d1e4b56c2",
+    "url": "/utsWeb/static/js/2.fe684f2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/utsWeb/static/js/2.fe684f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a859cbfbd11c1ef2c4",
+    "url": "/utsWeb/static/js/main.81206304.chunk.js"
+  },
+  {
+    "revision": "f1160b4f75db2e6a7aee",
+    "url": "/utsWeb/static/js/runtime-main.2f2a4f02.js"
+  }
+]);
